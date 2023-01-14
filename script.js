18aab65fd9356ca2f5d4a24f65e917421864543e
bluefortythree <117217594+bluefortythree@users.noreply.github.com>
2023-01-13 21:51:07 -0800

    finish first step of grid

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ const div = document.getElementById('parent')
 
 for (let i=0; i<16; i++) {
     const row = document.createElement('div')
-    row.innerText = "I am a row"
     row.classList.add('row')
     div.append(row)
     for (let i=0; i<16; i++) {
         const column = document.createElement('div')
-        column.innerText = "I am a column"
         column.classList.add('column')
         row.append(column)
     }
