2f2becb7289bdd66126013a6845a9dd6894226c6
bluefortythree <117217594+bluefortythree@users.noreply.github.com>
2023-01-14 23:25:53 -0800

    finish user grid size selection

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 const body = document.body
 const div = document.getElementById('parent')
 
-for (let i=0; i<16; i++) {
+let width = makeGrid()
+function makeGrid() {
+    for (i=0; i<1; i++) {
+        const gridSize = prompt("Please choose a number from 1-100 to be your grid's height/width:")
+        if(gridSize > 0 && gridSize < 101 && gridSize % 1 === 0) {
+            return gridSize;
+        } else {
+            alert("Please enter a valid number!")
+            i--;
+        }
+    }
+}
+
+console.log(width);
+
+for (let i=0; i<width; i++) {
     const row = document.createElement('div')
     row.classList.add('row')
     div.append(row)
-    for (let i=0; i<16; i++) {
+    for (let i=0; i<width; i++) {
         const column = document.createElement('div')
         column.classList.add('column')
         row.append(column)
     }
 }
 
-for(let i=0; i<256; i++) {
-    const hover = document.getElementsByClassName('column')[i]; 
+
+for(let i=0; i<width*width; i++) {
+    const hover = document.getElementsByClassName('column')[i];
     hover.addEventListener('mouseover', () => {
-        hover.setAttribute('style', 'background-color: pink;')
+        hover.setAttribute('style', 'background-color: pink')
         })
     hover.addEventListener('click', () => {
-        hover.setAttribute('style', 'background-color: white;')
+        hover.setAttribute('style', 'background-color: white')
     })
     const button = document.querySelector('button');
     button.addEventListener('click', () => {
-        hover.setAttribute('style', 'background-color: white;')
+        hover.setAttribute('style', 'background-color: white')
     })
 }
 
