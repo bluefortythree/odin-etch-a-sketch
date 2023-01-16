80f4afa1ce79b6ee82f889f563f2e3226cdff0bf
bluefortythree <117217594+bluefortythree@users.noreply.github.com>
2023-01-15 22:06:45 -0800

    add dynamic grid size

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const body = document.body
 const div = document.getElementById('parent')
 
+let initialSize = 960;
+
 let width = makeGrid()
 function makeGrid() {
     for (i=0; i<1; i++) {
@@ -16,29 +18,35 @@ function makeGrid() {
 
 console.log(width);
 
+let dimensions = initialSize/width+'px';
+console.log(dimensions);
+
 for (let i=0; i<width; i++) {
     const row = document.createElement('div')
     row.classList.add('row')
     div.append(row)
     for (let i=0; i<width; i++) {
         const column = document.createElement('div')
+        column.style.width = dimensions;
+        column.style.height = dimensions;
         column.classList.add('column')
         row.append(column)
     }
 }
 
 
+
+
 for(let i=0; i<width*width; i++) {
     const hover = document.getElementsByClassName('column')[i];
     hover.addEventListener('mouseover', () => {
-        hover.setAttribute('style', 'background-color: pink')
+        hover.style.backgroundColor = 'pink'
         })
     hover.addEventListener('click', () => {
-        hover.setAttribute('style', 'background-color: white')
+        hover.style.backgroundColor = 'white'
     })
     const button = document.querySelector('button');
     button.addEventListener('click', () => {
-        hover.setAttribute('style', 'background-color: white')
+        hover.style.backgroundColor = 'white'
     })
 }
-
